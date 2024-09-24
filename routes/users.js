0670a50f9766a7c3db05454ec8292b2e4083ec1d
gimminjae <min356812@naver.com>
2024-09-24 10:25:33 +0900

    user route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var router = express.Router()
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/**
+ * user apis
+ */
 
-module.exports = router;
+// get user by id
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource")
+})
+
+// create user ===== kakao, google, naver ... +
+router.post("/", function (req, res, next) {
+  res.send("respond with a resource")
+})
+
+// update user
+router.put("/", function (req, res, next) {
+  res.send("respond with a resource")
+})
+
+// delete user
+router.delete("/", function (req, res, next) {
+  res.send("respond with a resource")
+})
+
+module.exports = router
