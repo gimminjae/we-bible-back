b8712b5e1ff3dacd09d4a179bae0cfd5d559392a
gimminjae <min356812@naver.com>
2024-10-02 10:49:46 +0900

    remove ts

--- member/type/member.ts
+++
@@ -1,8 +0,0 @@
-interface Member {
-    id?: string
-    name: string
-    auth: string
-    createdDateTime: string
-    updatedDateTime: string
-    loginType: 'KAKAO' | 'NAVER' | 'GOOGLE' | 'NONE'
-}
